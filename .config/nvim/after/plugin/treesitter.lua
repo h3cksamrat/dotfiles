ff9f82714825b90f7c30c0d3c8260899163fe669
h3cksamrat (mint) <53104092+h3cksamrat@users.noreply.github.com>
2023-03-25 14:05:41 +0545

    updated: treesitter to only install required

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,31 +1,31 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "all",
-  sync_install = false,
-  highlight = { enable = true, additional_vim_regex_highlighting = false, },
-  rainbow = {
-    enable = true,
-  },
-  textobjects = {
-    move = {
-      enable = true,
-    },
-    select = {
-      enable = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      }
-    }
-  },
-  refactor = {
-    navigation = {
-      enable = true,
-      keymaps = {
-        goto_next_usage = "<A-*>",
-        goto_previous_usage = "<A-#>",
-      },
-    },
-  },
+	ensure_installed = { "typescript", "javascript", "lua", "c", "json", "yaml", "rust" },
+	sync_install = false,
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
+	rainbow = {
+		enable = true,
+	},
+	textobjects = {
+		move = {
+			enable = true,
+		},
+		select = {
+			enable = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+			},
+		},
+	},
+	refactor = {
+		navigation = {
+			enable = true,
+			keymaps = {
+				goto_next_usage = "<A-*>",
+				goto_previous_usage = "<A-#>",
+			},
+		},
+	},
 })
