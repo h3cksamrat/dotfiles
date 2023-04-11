fd1033bd3fcf121fe3aced532ff1d781d9a42830
h3cksamrat (mint) <53104092+h3cksamrat@users.noreply.github.com>
2023-04-11 13:16:04 +0545

    refactor: plugin loading stage

--- .config/nvim/lua/h3cksamrat/packer.lua
+++ .config/nvim/lua/h3cksamrat/packer.lua
@@ -1,87 +1,87 @@
 return require("packer").startup(function(use)
-  -- packer can manage itself
-  use("wbthomason/packer.nvim")
+	-- packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use("github/copilot.vim")
+	use("github/copilot.vim")
 
-  -- misc plugins
-  use("folke/zen-mode.nvim")
-  use("kyazdani42/nvim-web-devicons")
-  use("sbdchd/neoformat")
-  use("mbbill/undotree")
-  use("lukas-reineke/indent-blankline.nvim")
-  use("p00f/nvim-ts-rainbow")
+	-- misc plugins
+	use("folke/zen-mode.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("sbdchd/neoformat")
+	use("mbbill/undotree")
+	use("lukas-reineke/indent-blankline.nvim")
+	use("p00f/nvim-ts-rainbow")
 
-  use("lewis6991/gitsigns.nvim")
-  use({
-    "kylechui/nvim-surround",
-    tag = "*",
-  })
-  use("numToStr/Comment.nvim")
-  use("vimwiki/vimwiki")
+	use("lewis6991/gitsigns.nvim")
+	use({
+		"kylechui/nvim-surround",
+		tag = "*",
+	})
+	use("numToStr/Comment.nvim")
+	use("vimwiki/vimwiki")
 
-  -- colorsscheme
-  use("folke/tokyonight.nvim")
+	-- colorsscheme
+	use("folke/tokyonight.nvim")
 
-  -- tpope plugins
-  use("tpope/vim-repeat")
+	-- tpope plugins
+	use("tpope/vim-repeat")
 
-  -- a better status line
-  use("nvim-lualine/lualine.nvim")
+	-- a better status line
+	use("nvim-lualine/lualine.nvim")
 
-  use("s1n7ax/nvim-terminal")
-  use("kyazdani42/nvim-tree.lua")
+	-- use("s1n7ax/nvim-terminal")
+	use("kyazdani42/nvim-tree.lua")
 
-  -- telescope
-  use("nvim-lua/plenary.nvim")
-  use("nvim-lua/popup.nvim")
-  use("nvim-telescope/telescope.nvim")
+	-- telescope
+	use("nvim-lua/plenary.nvim")
+	use("nvim-lua/popup.nvim")
+	use("nvim-telescope/telescope.nvim")
 
-  -- treesitter
-  use("nvim-treesitter/nvim-treesitter")
-  use("romgrk/nvim-treesitter-context")
-  use("nvim-treesitter/nvim-treesitter-textobjects")
+	-- treesitter
+	use("nvim-treesitter/nvim-treesitter")
+	use("romgrk/nvim-treesitter-context")
+	use("nvim-treesitter/nvim-treesitter-textobjects")
 
-  -- lsp
-  use("williamboman/mason.nvim")
-  use("williamboman/mason-lspconfig.nvim")
-  use("neovim/nvim-lspconfig")
-  use("onsails/lspkind.nvim")
+	-- lsp
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("onsails/lspkind.nvim")
 
-  -- nvim-cmp
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
+	-- nvim-cmp
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
 
-  -- Luasnip
-  use("L3MON4D3/LuaSnip")
-  use("saadparwaiz1/cmp_luasnip")
+	-- Luasnip
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
 
-  -- Debugger
-  use("mfussenegger/nvim-dap")
-  use("rcarriga/nvim-dap-ui")
-  use("theHamsta/nvim-dap-virtual-text")
-  use("nvim-telescope/telescope-dap.nvim")
-  use("ravenxrz/DAPInstall.nvim")
+	-- Debugger
+	use("mfussenegger/nvim-dap")
+	use("rcarriga/nvim-dap-ui")
+	use("theHamsta/nvim-dap-virtual-text")
+	use("nvim-telescope/telescope-dap.nvim")
+	use("ravenxrz/DAPInstall.nvim")
 
-  use("mfussenegger/nvim-dap-python")
-  use("leoluz/nvim-dap-go")
+	use("mfussenegger/nvim-dap-python")
+	use("leoluz/nvim-dap-go")
 
-  -- language support
-  use("pangloss/vim-javascript") -- js support
-  use("leafgarland/typescript-vim") -- ts support
-  use("maxmellon/vim-jsx-pretty") -- JS and JSX syntax
-  -- use("jparise/vim-graphql") -- GraphQL syntax
-  use("mattn/emmet-vim")
+	-- language support
+	use("pangloss/vim-javascript") -- js support
+	use("leafgarland/typescript-vim") -- ts support
+	use("maxmellon/vim-jsx-pretty") -- JS and JSX syntax
+	-- use("jparise/vim-graphql") -- GraphQL syntax
+	use("mattn/emmet-vim")
 
-  -- dart
-  use("dart-lang/dart-vim-plugin")
-use {
-    'akinsho/flutter-tools.nvim',
-    requires = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-}
+	-- dart
+	use("dart-lang/dart-vim-plugin")
+	use({
+		"akinsho/flutter-tools.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+	})
 end)
